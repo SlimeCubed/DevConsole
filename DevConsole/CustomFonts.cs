ade784fbcf03e50b7f6d1aceb17adaf9415a8314
Dual <31146412+Dual-Iron@users.noreply.github.com>
2021-07-11 17:02:43 -0400

    Update CustomFonts.cs to allow hot reloading

--- DevConsole/CustomFonts.cs
+++ DevConsole/CustomFonts.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -22,6 +22,10 @@ namespace DevConsole
 
         private static FFont LoadFont(string name, string configPath, float offsetX, float offsetY, FTextParams textParams)
         {
+            if (Futile.atlastManager._fontsByName.TryGetValue(name, out var possibleFont)) 
+            {
+                return possibleFont;
+            }
 			FFont font;
             FAtlasElement element;
             Assembly asm = Assembly.GetExecutingAssembly();
