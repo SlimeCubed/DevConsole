5c37dac70f14fd48e5ddb10a1ce6539c0d3283d8
Dual-Iron <31146412+Dual-Iron@users.noreply.github.com>
2021-07-29 17:51:16 -0400

    Rename Position.cs

--- DevConsole/Positioning.cs
+++ DevConsole/Positioning.cs
@@ -3,7 +3,7 @@
     /// <summary>
     /// Contains methods for getting an in-game position from a position string.
     /// </summary>
-    public static class Position
+    public static class Positioning
     {
         public static RoomPos GetPosition(RainWorldGame game, string arg)
         {
