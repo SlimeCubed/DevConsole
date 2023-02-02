4f5ad7c657a1f7a0d4d06e0047219a0f7e4107bd
SlimeCubed <cubedslime@gmail.com>
2023-02-01 17:07:17 -0800

    Bump version

--- DevConsole/DevConsoleMod.cs
+++ DevConsole/DevConsoleMod.cs
@@ -17,7 +17,7 @@ namespace DevConsole
     internal class DevConsoleMod : BaseUnityPlugin
     {
         public const string MOD_ID = "slime-cubed.devconsole";
-        public const string MOD_VERSION = "1.1.4";
+        public const string MOD_VERSION = "1.2.0";
         private static bool initialized = false;
 
         public void Awake()
